2dde6ad42065986a711181a413ae40b49ddff8fe
qoridze <qoridzenikoloz66@gmail.com>
2024-09-13 23:26:31 +0400

    first commit

---
+++ level 01/homework/homework.py
@@ -0,0 +1,34 @@
+name = "nika"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშველობის მიმნიჭებელი სიმბოლო
+#"nika"არის ცვლადისთვის მნიშვნელოვანი
+
+surname ="qoridze"
+
+print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტი
+
+neme ="nika"# ეს არის str(string) ტიპის ცვლადი
+age = 22 # ეს არის int (integer) მტელი რიცხვი
+height = 184.5 #ეს არის float ტიპის ცვლადი (ათწილადი)
+#boolean (bool) ტიპის ცვლადი
+
+knows_programing = True # True     false
+is_ugly = False #snakecase(უბრალოდ წერის სტილი სტანდარტულად)
+
+isUgly = False # ჯავასკრიპტული camelcase
+
+
+#print(name +" " + surname)
+
+#სტრინგი არის ბრჭყალებში მოქცეული სიმბოლოები
+#print(name + age)
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programing))
+
+print(name +" " + str (age))
+
