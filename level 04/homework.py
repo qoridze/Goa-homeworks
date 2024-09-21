8d45c23b7f6bc40134db356120c00fab20aa278c
qoridze <qoridzenikoloz66@gmail.com>
2024-09-21 17:52:46 +0400

    second commit

---
+++ level 04/homework.py
@@ -0,0 +1,26 @@
+# bagi igive anu rame shecdoma rom gvaqvs da rom unda gamovaswort
+
+# Print es ar sheizleba ase seizleba print
+
+
+num1 = 16
+num2 = 17
+
+print(num1 + num2)
+print(num1 - num2)
+print(num1 * num2)
+print(num1 / num2)
+
+
+
+age = 13
+age1 =  11
+age2 = 38
+age3 = 40
+
+print(age + age1 + age2 + age3)
+
+
+
+
+
