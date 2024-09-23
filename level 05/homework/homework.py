ddfff39824d374b0ba9732ed28b8ba7d7687e51d
qoridze <qoridzenikoloz66@gmail.com>
2024-09-23 20:22:43 +0400

    second commit

---
+++ level 05/homework/homework.py
@@ -0,0 +1,42 @@
+#1)მომხმარებელს შემოატანინეთ float სახის რიცხვი და შემდეგ ეგ რიცხვი გაყავით floor division-ის დახმარებით რომ იყოს integer და არა float
+num1 = float(input("please enter float: "))
+
+num2 = float(input("please enter second float: "))
+print(int(num1 / num2))
+
+#2)მოხმარებელს შემოატანინეთ თავის სახელი,გვარი,ასაკი,ჰობბი და ბალანსი შემდეგ ეს ყველაფერი დაბეჭდეთ print-ის საშუალებით
+
+name = input("please enter name: ")
+
+surname = input("please enter surname: ")
+
+age = input("please enter age: ")
+
+hobby = input("please enter hobby: ")
+
+print("user name is ", name)
+print("user surname is ", surname)
+print("user age is ", age)
+print("user hobby is ", hobby)
+
+#3)გააკეთეთ 5 5-ი მაგალითი %-ზე და floor division-ზე //
+print(874 % 79)
+print(634 % 42)
+print(442 % 41)
+print(833 % 53)
+print(544 % 353)
+
+#4)მოხმარებელს შემოატანინეთ რიცხვი და მაგ რიცხვს გამოაკელით 20 და გაამრავლეთ 3 ზე
+
+num4 = int(input("please enter number: "))
+
+print(num4 - 20 * 3)
+
+#5)მოხმარებელს შემოატანინეთ რიცხვები ანუ num,num1,num2...და ასე შემდეგ და საბოლოოდ გამოითვალეთ მაგ რიცხების საშუალო არითმეტიკული
+
+num5 = int(input("please enter number: "))                                                                    
+num6 = int(input("please enter number"))
+num = int(input("please enter number")) 
+
+print(num5 + num6 + num)
+print(10821 / 3)
