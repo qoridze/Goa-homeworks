e09558782a54a38a636cff116437ac92706f2e63
qoridze <qoridzenikoloz66@gmail.com>
2024-10-08 16:52:45 +0400

    second commit

---
+++ level 09/classwork/classwork.py
@@ -0,0 +1,11 @@
+age = int(input("enter your age: "))
+print(age >= 18)
+
+name =(input(" enter your name: "))
+
+print("nika" == "nika")
+
+num1 =(input("enter your number: "))
+
+number = int(input("enter your number: "))
+print(number % 2 == 0)
