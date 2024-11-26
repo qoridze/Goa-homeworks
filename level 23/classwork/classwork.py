9cde7dadb6b3adba6198474bf5b81542db971cd8
qoridze <qoridzenikoloz66@gmail.com>
2024-11-26 14:24:41 +0400

    second commit

---
+++ level 23/classwork/classwork.py
@@ -0,0 +1,16 @@
+# 1)კომენტარის მეშვეობით ახსენით .lower() / .upper() / .capitalize() / .find() ფუნქციების დანიშნულება
+# lower aris patara asoebad gamotana  upper aris yvela didi aso capitalize aris pirveli didi aso da find ari anu erti aso romel indexze aris
+# 2)მომხმარებელს შემოატანინეთ მისი გვარი და შეამოწმეთ, თუ გვარის პირველი ასო არის დიდი ასო, მაშინ დაუბეჭდეთ "Hello",
+#  თუ არ არის მაშინ დაუბეჭდეთ "Bye"
+surname  = input("please enter your surname: ")
+print("bye")
+# 3)მომხმარებელს შემოატანინეთ სახელი, შემდეგ შემოატანინეთ ასო და თუ ამ სახელში ეს ასო არიქნება, მაშინ გამოუტანეთ 
+# "Can't find character", თუ იქნება მაშინ გამოუტანეთ "found it" და გვერდზე მიუწერეთ ამ ასოს ინდექსი
+name = "nika"
+name = input("please enter your name: ")
+print("j")
+print("can not find character")
+# 4)მომხმარებელს შემოატანინეთ მისი გვარი, შემდეგ შეეკითხეთ როგორ უნდა რო შეიცვალოს მისი გვარის ასოები, თუ გიპასუხებთ "upper" გადაიყვანეთ მთლიანი
+#  გვარი დიდ ასოებად,თუ გიპასუხებთ "lower" გადაიყვანეთ მთლიანი გვარი პატარა ასოებად და თუ გიპასუხებთ "capitalize" გადაიყვანეთ გვარის მხოლოდ პირველი ასო დიდ ასოდ.
+surname = input("please enter your surname: ")
+print(surname.upper())
