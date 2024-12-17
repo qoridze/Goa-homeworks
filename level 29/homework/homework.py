bd44a6c160cc9de3d92dc3eb59d90e4062d5fa29
qoridze <qoridzenikoloz66@gmail.com>
2024-12-17 14:17:23 +0400

    second commit

---
+++ level 29/homework/homework.py
@@ -0,0 +1,7 @@
+# 1)შექმენით ფუნქცია რომელსაც გადაეცემა ორი რიცხვი შემდეგ დააბრუნეთ მათი ნამრავლი return ის გამოყენებით 
+numbers = (34,242)
+print(34 * 242)
+# 2)ახსენით რაც დღეს გავიარეთ (isalnum,isdigit,islower,isnumeric,isupper,count) მეთოდები თითოეული რას აკეთებს.
+
+# 3)ასევე ახსენით რას აკეთებს min და max ჩაშენებული ფუნქციები
+# min afataravebs texts da max adidebs texts 
