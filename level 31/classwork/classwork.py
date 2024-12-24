3fab9b51687b7c8c7bb03a05fb4fd522414f7d5d
qoridze <qoridzenikoloz66@gmail.com>
2024-12-24 19:47:47 +0400

    second commit

---
+++ level 31/classwork/classwork.py
@@ -0,0 +1,9 @@
+
+# Convert boolean values to strings 'Yes' or 'No'.
+
+def bool_to_word(boolean):
+    if boolean == True:
+        return "Yes"
+    else:
+        return "No"
+    
---
+++ level 31/homework/homework.py
@@ -0,0 +1,11 @@
+# https://www.codewars.com/kata/5168bb5dfe9a00b126000018/train/python
+
+# https://www.codewars.com/kata/544675c6f971f7399a000e79/train/python
+
+# https://www.codewars.com/kata/551b4501ac0447318f0009cd/train/python
+
+# https://www.codewars.com/kata/57a0556c7cb1f31ab3000ad7/train/python
+
+# https://www.codewars.com/kata/53369039d7ab3ac506000467/train/python
+
+# https://www.codewars.com/kata/56bc28ad5bdaeb48760009b0/train/python
