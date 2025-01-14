1fd9f7dbf01326db8e0d2f43028691eb4ee6f28b
qoridze <qoridzenikoloz66@gmail.com>
2025-01-14 20:54:51 +0400

    second commit

---
+++ level 37/homework/homework.py
@@ -0,0 +1,17 @@
+# https://www.codewars.com/kata/570597e258b58f6edc00230d/train/python
+
+# https://www.codewars.com/kata/55a14f75ceda999ced000048/train/python
+
+# https://www.codewars.com/kata/596fba44963025c878000039/train/python
+
+# https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/train/python
+
+# https://www.codewars.com/kata/562d8d4c434582007300004e/train/pythonhttps://www.codewars.com/kata/570597e258b58f6edc00230d/train/python
+
+# https://www.codewars.com/kata/55a14f75ceda999ced000048/train/python
+
+# https://www.codewars.com/kata/596fba44963025c878000039/train/python
+
+# https://www.codewars.com/kata/57a1fd2ce298a731b20006a4/train/python
+
+# https://www.codewars.com/kata/562d8d4c434582007300004e/train/python
