19758e029c16e8c548e03814141eef89ad189c10
qoridze <qoridzenikoloz66@gmail.com>
2025-03-01 20:54:49 +0400

    second commit

---
+++ level 51/homework/homework.py
@@ -0,0 +1,20 @@
+# 1) კომენტარის სახით ახსენით, თუ რა განასხვავებს tuple-ებს List-ებისგან.
+# turpleti cven segvidlia rom siidan ragac nawili shevcvalot da ara marto sevcvalot amovigot kidec
+# 2) შექმენით tuple, რომელიც შეიცავს თქვენს 5 საყვარელ ფილმს, შემდეგ კი დაბეჭდეთ პირველი და ბოლო ელემენტი ამ tuple-დან.
+films =["teen wolf","prison break","vikings","harry potter","breaking bad"]
+films[0,4]
+print(films)
+# 3) შემენით Tuple, რომელშიც შეინახავთ კვირის დღეებს და მოახდინეთ მათი unpacking (destruction),რათა მიანიჭოთ თითოეული
+#  ელემენტი ცალკეულ ცვლადებს და დაბეჭდეთ ეს ცვლადები.
+kviris_dgeebi =["ორშაბათი","სამშაბათი","ოთხშაბათი","ხუტშაბათი","პარასკევი","შაბათი","კვირა"]
+print(kviris_dgeebi)
+# 4) შექმენით tuple, რომელიც შეიცავს რამდენიმე ელემენტს. შემდეგ შეამოწმეთ, შეიცავს თუ არა ეს tuple კონკრეტულ ელემენტს
+
+# 5) მოცემულია tuple = ("banana", "cherry", "strawberry", "raspberry")
+# (first, *second, third) = fruits
+
+# რას გამოიტანს შემდეგი კოდი?
+# print(first)
+# print(second)
+# print(third)
+# es kodi gamoitans: cherry, strawberry, rspberry
